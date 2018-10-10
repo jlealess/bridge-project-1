9b1f88eafbdd712f5be77ff9d611e68cc4bb6ed2
Jackie Lealess <jlealess@gmail.com>
2018-10-10 06:49:56 -0400

    Changing button styles

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = props => (
-    <button onClick={props.handleClick}>{props.value}</button>
+    <button onClick={props.handleClick} className={props.className}>{props.value}</button>
 );
 
 export default Button;
--- src/Profile.js
+++ src/Profile.js
@@ -4,13 +4,17 @@ import FollowingList from "./FollowingList";
 import Button from "./Button.js";
 
 export default ({ login, avatar_url, handleLogout, events, followers }) => (
-    <div className="profile">
-        <div>
-            <h2>Hello, {login}</h2>
-            <img src={avatar_url} alt={`${login}`} />
-            <Button handleClick={handleLogout} value="Logout" />
-            <FollowingList followers={followers} />
-        </div>
-        <EventList events={events} />
+  <div className="profile">
+    <div>
+      <h2>Hello, {login}</h2>
+      <img src={avatar_url} alt={`${login}`} className="profile__avatar" />
+      <Button
+        handleClick={handleLogout}
+        value="Logout"
+        className="full-width"
+      />
+      <FollowingList followers={followers} />
     </div>
-)
+    <EventList events={events} />
+  </div>
+);
