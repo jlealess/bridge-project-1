406a63dd5bd776136526535505dfbaa9bf437faa
Jackie Lealess <jlealess@gmail.com>
2018-11-11 16:21:35 -0500

    Updates

---
+++ precache-manifest.93f94d0079612dcd923cace7bce8f54d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98517e5bf41b238561e6",
+    "url": "/bridge-project-1/static/js/runtime~main.98517e5b.js"
+  },
+  {
+    "revision": "c3ea8c2b5f077fdc6b6f",
+    "url": "/bridge-project-1/static/js/main.c3ea8c2b.chunk.js"
+  },
+  {
+    "revision": "3b47519674fc6d791958",
+    "url": "/bridge-project-1/static/js/1.3b475196.chunk.js"
+  },
+  {
+    "revision": "c3ea8c2b5f077fdc6b6f",
+    "url": "/bridge-project-1/static/css/main.6bdc5d64.chunk.css"
+  },
+  {
+    "revision": "804ccba022ddefafbd58bcbd5e7d0496",
+    "url": "/bridge-project-1/index.html"
+  }
+];
